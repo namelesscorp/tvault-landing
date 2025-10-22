56e37c8218d36dbc46566f66d8229a18c8133309
Dmitriy Stepanov <stdian@mail.ru>
2025-10-22 23:25:08 +0800

    [LANDING-13] Редизайн футера

---
+++ src/components/LayoutFooter/LayoutFooter.model.ts
@@ -0,0 +1,89 @@
+export const linksCategories = ["navigation", "resources", "socials"];
+
+export const links = [
+	[
+		{
+			titleKey: "overview",
+			href: "#",
+		},
+		{
+			titleKey: "download",
+			href: "#",
+		},
+		{
+			titleKey: "roadmap",
+			href: "#",
+		},
+		{
+			titleKey: "features",
+			href: "#",
+		},
+		{
+			titleKey: "versions",
+			href: "#",
+		},
+	],
+	[
+		{
+			titleKey: "documentation",
+			href: "#",
+		},
+		{
+			titleKey: "comparisons",
+			href: "#",
+		},
+		{
+			titleKey: "community",
+			href: "#",
+		},
+		{
+			titleKey: "nameless",
+			href: "#",
+		},
+		{
+			titleKey: "pressKit",
+			href: "#",
+		},
+		{
+			titleKey: "contact",
+			href: "#",
+		},
+		{
+			titleKey: "reviews",
+			href: "#",
+		},
+		{
+			titleKey: "faq",
+			href: "#",
+		},
+	],
+	[
+		{
+			titleKey: "x",
+			href: "https://t.me/tvault_app",
+		},
+		{
+			titleKey: "telegram",
+			href: "https://x.com/tvault_app",
+		},
+		{
+			titleKey: "github",
+			href: "https://github.com/namelesscorp/",
+		},
+	],
+];
+
+export const bottomLinks = [
+	{
+		titleKey: "privacyPolicy",
+		href: "#",
+	},
+	{
+		titleKey: "termsOfService",
+		href: "#",
+	},
+	{
+		titleKey: "license",
+		href: "#",
+	},
+];
--- src/components/LayoutFooter/LayoutFooter.tsx
+++ src/components/LayoutFooter/LayoutFooter.tsx
@@ -2,53 +2,72 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
+import { bottomLinks, links, linksCategories } from "./LayoutFooter.model";
+
 const LayoutFooter = () => {
 	const t = useTranslations("HomePage.footer");
 	return (
-		<footer className="px-[20px] py-[40px] lg:px-[40px]  bg-[#08091C]">
-			<div className="flex flex-col-reverse lg:flex-row lg:justify-between gap-[60px] lg:gap-[0px]">
-				<div className="flex flex-col gap-[12px] lg:gap-[20px]">
-					<p className="font-inter lg:uppercase font-medium text-[22px] leading-[134%] tracking-[0.007em] lg:text-[40px] lg:font-black text-[#FFFFFF] lg:leading-[100%]">
-						{t("questions.1")}
-						<br />
-						{t("questions.2")}
+		<footer className="px-[20px] lg:px-[50px] py-[20px] bg-[#101827] flex flex-col gap-[20px] lg:gap-[50px]">
+			<div className="flex flex-col gap-[30px] lg:gap-[0px] lg:flex-row justify-between lg:px-[30px]">
+				<div className="flex flex-col gap-[12px] lg:gap-[20px] lg:max-w-[430px]">
+					<div className="flex items-center gap-[15px]">
+						<Image src="/logo.svg" alt="logo" width={60} height={60} className="rounded-[10px]" />
+						<p className="font-inter font-extrabold text-white tracking-[-0.05em] text-[24px]">
+							Trust Vault
+						</p>
+					</div>
+					<p className="font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
+						Your personal data safe designed with security, privacy, and convenience in mind. Open source,
+						locally controlled, and built for trust.
 					</p>
 					<a
 						href="mailto:support@tvault.app"
-						className="font-inter lg:uppercase font-medium text-[22px] leading-[134%] tracking-[0.007em] lg:text-[40px] lg:font-black text-[#1648F9] lg:leading-[100%] underline underline-offset-[6px] lg:underline-offset-[10px]">
+						className="font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
 						support@tvault.app
 					</a>
-				</div>
-				<div className="flex flex-col lg:flex-row gap-[24px] lg:gap-[20px]">
-					<a
-						href="https://t.me/tvault_app"
-						target="_blank"
-						className="font-inter font-medium text-[22px] leading-[134%] tracking-[0.007em] lg:text-[18px] lg:leading-[144%]  text-[#FFFFFF]">
-						Telegram
-					</a>
-					<a
-						href="https://x.com/tvault_app"
-						target="_blank"
-						className="font-inter font-medium text-[22px] leading-[134%] tracking-[0.007em] lg:text-[18px] lg:leading-[144%]  text-[#FFFFFF]">
-						X
-					</a>
-					<a
-						href="https://github.com/namelesscorp/"
-						target="_blank"
-						className="font-inter font-medium text-[22px] leading-[134%] tracking-[0.007em] lg:text-[18px] lg:leading-[144%]  text-[#FFFFFF]">
-						GitHub
-					</a>
-				</div>
-			</div>
-			<div className="flex justify-between items-end mt-[60px] lg:mt-[180px]">
-				<Image src="/nameless_logo.svg" alt="Nameless" width={98} height={65} />
-				<div className="font-inter font-medium text-[18px] leading-[124%] tracking-[0.007em] text-[rgba(255,255,255,0.6)] text-right">
-					<p>
+					<p className="font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
 						<Link href="/en">EN</Link> / <Link href="/ru">RU</Link>
 					</p>
-					{/* <Link href="#" className="hidden lg:block mt-[26px]">Политика обработки персональных данных</Link> */}
-					<p className="mt-[20px] lg:mt-[26px]">&copy; 2025 {t("copyright")}</p>
 				</div>
+				{linksCategories.map((category, index) => (
+					<div key={category} className="flex flex-col gap-[14px] lg:gap-[35px]">
+						<p className="font-inter font-extrabold text-[24px] tracking-[-0.05em] text-white">
+							{t(`links.${category}.title`)}
+						</p>
+						<div className="flex flex-col gap-[8px] lg:gap-[15px]">
+							{links[index].map(link => (
+								<Link
+									key={link.titleKey}
+									href={link.href}
+									className="font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
+									{t(`links.${category}.${link.titleKey}`)}
+								</Link>
+							))}
+						</div>
+					</div>
+				))}
+			</div>
+			<div className="w-full h-[1px] bg-white/20"></div>
+			<div className="flex flex-col lg:flex-row gap-[20px] lg:gap-[0px] lg:justify-between lg:items-center">
+				<div className="flex lg:flex-row gap-[40px] items-center">
+					<Image src="/nameless_logo.svg" alt="Nameless" width={98} height={65} />
+					<p className="hidden lg:block font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
+						© 2025 Trust Vault. {t("copyright")}
+					</p>
+				</div>
+				<div className="flex flex-col lg:flex-row gap-[10px] lg:gap-[15px] lg:items-center">
+					{bottomLinks.map(link => (
+						<Link
+							key={link.titleKey}
+							href={link.href}
+							className="font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
+							{t(`links.bottom.${link.titleKey}`)}
+						</Link>
+					))}
+				</div>
+				<p className="block lg:hidden font-inter font-medium text-[16px] lg:text-[20px] tracking-[-0.05em] text-white/70">
+					© 2025 Trust Vault. {t("copyright")}
+				</p>
 			</div>
 		</footer>
 	);
